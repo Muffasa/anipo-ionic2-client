e2e31ad9191e525749e796279bb0a287ed650902
muffasa <ohadgalor1990@gmail.com>
2016-02-13 21:05:36 +0200

    gps and bluetooth cordova plugins and example pages added

--- app/pages/page3/page3.ts
+++ app/pages/page3/page3.ts
@@ -8,4 +8,50 @@ export class Page3 {
   constructor() {
 
   }
+  deviceUuid = device? device.uuid:"device is undefined"
+
+    checkBT = () => {
+         console.log("entered checkBT")
+            cordova.plugins.diagnostic.isBluetoothEnabled(function(enabled){
+                console.log("Bluetooth is " + (enabled ? "enabled" : "disabled"));
+            }, function(error){
+                console.error("The following error occurred: "+error)
+            });      
+    }
+    switchToBluetoothSettings = () => {
+         console.log("entered switchToBluetoothSettings")
+            cordova.plugins.diagnostic.switchToBluetoothSettings()    
+    }
+    list = () => {
+         console.log("entered list")
+        bluetoothSerial.list(
+            function(results) {
+                console.log(JSON.stringify(results));
+            },
+            function(error) {
+                console.log(JSON.stringify(error));
+            }
+        );   
+    }
+    discoverUnpaired = () =>{
+        console.log("entered discoverUnpaired")
+            function success(retData) {
+            console.log("entered initializeSuccess")
+            debugger
+                console.log("retData: " + retData)
+            }
+
+            function failure(error) {
+                console.log("entered initializeError")
+                console.log('code: '    + error.code    + '\n' +
+                    'message: ' + error.message + '\n');
+            }
+            let params = {
+                            "request": true,
+                            "statusReceiver": false
+                         }
+
+           bluetoothSerial.discoverUnpaired(success, failure);
+    }
+
 }
