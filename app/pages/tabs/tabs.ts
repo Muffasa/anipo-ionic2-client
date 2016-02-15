aaadd2fbc0afba6ed89fd0a6adec5412cd5bced7
muffasa <ohadgalor1990@gmail.com>
2016-02-15 14:20:01 +0200

    anipo tab added

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -2,7 +2,7 @@ import {Page} from 'ionic-framework/ionic';
 import {Page1} from '../page1/page1';
 import {Page2} from '../page2/page2';
 import {Page3} from '../page3/page3';
-
+import {AnipoPage} from '../anipo/anipo';
 // https://angular.io/docs/ts/latest/api/core/Type-interface.html
 import {Type} from 'angular2/core';
 
@@ -16,7 +16,8 @@ export class TabsPage {
   tab1Root: Type = Page1;
   tab2Root: Type = Page2;
   tab3Root: Type = Page3;
-
+  tab4Root: Type = AnipoPage;
+  
   constructor() {
 
   }
